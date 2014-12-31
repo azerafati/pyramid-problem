b73f29f4e81cb40675f6d1eab2c41fd7c63afce8
Alireza <bludream@gmail.com>
2014-12-31 11:52:54 +0330

    tested basics

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
-$(function(){
+$(function() {
 
 
-	function start (){
-		var pyramid={
-				month=0,
-				members= $('[name=start]').val()
-
-		} 
-		var totalpop = $('[name=population]').val();
-		while(pyramid<totalpop){
-			addRow([month,members]);
+	
+	///main class
+	function Pyramid () {
+		this.month = 1;
+		this.members = eval($('[name=start]').val());
+		this.activeMembers = this.members;
+		this.insertion = eval($('[name=insertion]').val());
+	}
+	
+	
+	
+	
+	
+	function start() {
+		
+		$('table tbody tr').remove();
+		
+		var pyramid = new Pyramid();
+		
+		var totalpop = eval($('[name=population]').val());
+		
+		while (pyramid.members < totalpop) {
+			addRow([ pyramid.month, pyramid.members ]);
+			pyramid.members += pyramid.activeMembers * pyramid.insertion;
+			pyramid.month++;
 		}
 	}
 
-	function addRow(items){
+	function addRow(items) {
 		var row = $('<tr>');
 		var cell = $('<td>');
-		for(var item in items){
-			row.append(cell.clone.text(item));
-		}
+		items.forEach(function(item) {
+			row.append(cell.clone().text(item));
+		});
 		$('table tbody').append(row);
 	}
 
-	$('#start').click(){
+	$('#start').click(function() {
 		console.log('starting');
 		start();
-	}
+	})
 });
